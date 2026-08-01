c8ae96fa70758b1f05ab13ff9f692c56da50aa3b
shahadat126 <mdshahadathossainzen@gmail.com>
2026-08-01 20:07:39 +0600

    BlogPosts

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post,Category
 from django.contrib.auth.models import User
 from .forms import PostCreatForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q,Count
 # Create your views here.
 def Home(request):
     try:
@@ -13,7 +14,23 @@ def Home(request):
 
 def posts_list_view(request):
     try:
-        posts = Post.objects.all()
+        query = request.GET.get('query','')
+        category_id = request.GET.get('category_id','')
+        status = request.GET.get('status','')
+        posts = Post.objects.select_related('category','author')
+        
+        if query:
+            posts = posts.filter(
+                Q(title__icontains = query)|
+                Q(content__icontains=query)|
+                Q(author__username__icontains = query)
+            )
+        
+        if category_id:
+            posts = posts.filter(category__id = category_id)
+        
+        if status:
+            posts = posts.filter(status = status)
             
         posts_summary_list= []
         for post  in posts:
@@ -21,10 +38,32 @@ def posts_list_view(request):
                 {
                     'id': post.id,
                     'title' : post.title,
-                    'content' : post.content[:100]
+                    'content' : post.content[:100],
+                    'category' : post.category_id,
+                    'created' : post.created
                 } 
                 )
-        return render(request,"blogapp/post_list_view.html",{'posts_summary_list':posts_summary_list})
+        my_posts = None
+        my_categories= None
+        if request.user.is_authenticated:
+            my_posts = Post.objects.filter(author=request.user).aggregate(total_posts= Count('id'))   
+            
+            my_categories= Category.objects.annotate(post_count= Count('posts')) 
+            
+            
+        return render(request,"blogapp/post_list_view.html",{
+            'posts_summary_list':posts_summary_list,
+            'categories': Category.objects.all(),
+            'status_choices' : Post.STATUS_CHOICES,
+            'query' : query,
+            'selected_category':category_id,
+            'selected_status' : status,
+            'my_posts': my_posts,
+            'my_categories' : my_categories,
+            'total_result': len(posts_summary_list)
+            
+            
+            })
     except Exception as e:
         return render(request,'blogapp/error.html',{'error':e})
 def post_details_view(request,pk):
